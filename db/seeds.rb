72c5522935d5dc324e4d554f5200ce87bb04e2a9
Mike Post <michael.post@appfolio.com>
2019-10-23 15:39:22 -0700

    mjp - Add Images seed for 20 images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+images = Image.create([
+  { url: 'https://peopledotcom.files.wordpress.com/2017/11/dog.jpg' },
+  { url: 'http://static.businessinsider.com/image/5484d9d1eab8ea3017b17e29/image.jpg' },
+  { url: 'https://designerdoginfo.files.wordpress.com/2012/04/puppy-and-adult-dog.jpg' },
+  { url: 'http://1.bp.blogspot.com/__82348puvBM/TACA7PXeDNI/AAAAAAAAAB4/DnVPw_0wnjw/s1600/golden-retriever-puppies.jpg' },
+  { url: 'http://3.bp.blogspot.com/-rBzLls3Dihs/ToVp-wM32vI/AAAAAAAAAhc/KMVYHQ8huq4/s1600/dogs6.jpg' },
+  { url: 'http://animalsbirds.com/wp-content/uploads/2017/08/Dog-animals-pictures-download.jpg' },
+  { url: 'https://1.bp.blogspot.com/-NL0QHNr7tSs/TpgHR7w8-aI/AAAAAAAAAqI/jY12zqL9CLU/s1600/20829205916-lovely+dog%2528vn801.com%2529.jpg' },
+  { url: 'http://1.bp.blogspot.com/-1uQRYMklACU/ToQ6aL-5uUI/AAAAAAAAAgQ/9_u0922cL14/s1600/cute-puppy-dog-wallpapers.jpg' },
+  { url: 'http://1.bp.blogspot.com/-kr19I7WtdKs/TixmaPnkDPI/AAAAAAAAANc/Kp6CNpUrwLU/s1600/4dogs_8578.jpg' },
+  { url: 'http://www.pets4homes.co.uk/images/articles/1717/large/the-pros-and-cons-of-taking-on-two-unrelated-adult-dogs-at-once-5393118897ebb.jpg' },
+  { url: 'http://www.smallworlddoggie.com/wp-content/uploads/2014/04/dogs1.jpg' },
+  { url: 'http://www.healthypawspetinsurance.com/blog/wp-content/uploads/dog_food_bowl_diet.jpg' },
+  { url: 'https://www.servicedogsforamerica.org/dogs/wp-content/uploads/2012/08/dog-faq.jpg' },
+  { url: 'http://statcdn.fandango.com/MPX/image/NBCU_Fandango/457/1019/ADogsPurpose_Clip_ChaseABall.jpg' },
+  { url: 'https://photokaz.com/wp-content/uploads/2012/05/2012-05-05-Shelter-Dogs-1356-MKH-Edit_mini.jpg' },
+  { url: 'https://thenypost.files.wordpress.com/2018/12/181229-dogs.jpg?quality=90&strip=all&w=1200' },
+  { url: 'https://www.dogsinmotion.com.au/wp-content/uploads/Dogs-in-Motion-Blog-Posts-1024x768.jpg' },
+  { url: 'https://www.guidedogs.org/wp-content/uploads/2018/01/Mobile.jpg' },
+  { url: 'https://images.wagwalkingweb.com/media/articles/dog/why-is-my-dog-always-hungry/why-is-my-dog-always-hungry.jpg' },
+  { url: 'https://www.lifewithdogs.tv/wp-content/uploads/2015/10/10.10.15-Dogs-Loving-Autumn17.jpg' }
+])
