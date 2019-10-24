a64ab8184a950c6d0a6b9909d3aacd648c14ecb8
Mike Post <michael.post@appfolio.com>
2019-10-23 17:46:18 -0700

    mjp - Make Image model taggable

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ApplicationRecord
   validates :url, presence: true, url: true
+
+  acts_as_taggable_on :tags
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,10 +1,21 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  def test_image__valid
+  def test_image__valid__no_tags
     image = Image.new(url: 'https://designerdoginfo.files.wordpress.com/2012/04/puppy-and-adult-dog.jpg')
 
     assert_predicate image, :valid?
+  end
+
+  def test_image__valid_with_tags
+    [%w[dogs], %w[puppy adult], %w[dog puppy awesome]].each do |tags|
+      image = Image.new(url: 'https://designerdoginfo.files.wordpress.com/2012/04/puppy-and-adult-dog.jpg')
+      tags.each do |tag|
+        image.tag_list.add tag
+      end
+
+      assert_predicate image, :valid?
+    end
   end
 
   def test_image__invalid__blank_url
