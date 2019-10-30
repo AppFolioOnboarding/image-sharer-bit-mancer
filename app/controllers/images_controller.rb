bc542c94e81ab685e16c5ad61552e607ce91a133
Mike Post <mike.post@appfolio.com>
2019-10-30 11:22:29 -0700

    mjp - Address review feedback

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -23,6 +23,8 @@ class ImagesController < ApplicationController
 
   def show
     @image = Image.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to images_path, notice: 'The image was not found.'
   end
 
   def destroy
@@ -30,6 +32,8 @@ class ImagesController < ApplicationController
     @image.destroy
 
     redirect_to images_path, notice: 'You have successfully deleted the image.'
+  rescue ActiveRecord::RecordNotFound
+    redirect_to images_path, notice: 'The image was not found.'
   end
 
   private
